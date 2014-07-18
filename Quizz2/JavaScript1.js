3d9654d955c3baa6718a5767bf7e63d14e7f0ff0
dmitry-goloveyko <d.goloveyko@gmail.com>
2014-07-18 16:10:33 +0300

    Quizz2

---
+++ Quizz2/JavaScript1.js
@@ -0,0 +1,5 @@
+﻿(function ($) {
+    $('*').error(function () {
+        $('body').prepend('<div id="errorMessage" style="position:fixed; float:right; background-color:red;">There is an unexpected error on page</div>');
+    });
+})(jQuery);
